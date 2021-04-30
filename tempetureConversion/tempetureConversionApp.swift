75376defdf05800a23d8ac0be96a23cc989084e9
eya nasri <eyanasri@think-it.com>
2021-04-30 09:54:59 +0100

    Initial Commit

---
+++ tempetureConversion/tempetureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempetureConversionApp.swift
+//  tempetureConversion
+//
+//  Created by Eya Nasri on 30/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct tempetureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
